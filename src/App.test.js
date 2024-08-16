4e950109da399375338f6995afe8d70c05bc3541
Anastasiya Lazarenko <anastasiya.lazarenko@ibm.com>
2024-08-16 12:31:40 -0400

    chore: unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import App from './App';
 import { HashRouter as Router } from 'react-router-dom';
-
 import { render, screen } from '@testing-library/react';
+
+import App from './App';
+import RepoPage from './content/RepoPage/RepoPage';
+import LandingPage from './content/LandingPage';
 
 describe('React Step 1 Tests', () => {
   it('renders without crashing', () => {
@@ -21,5 +23,21 @@ describe('React Step 1 Tests', () => {
     );
     const header = screen.getByRole('banner', { name: 'Carbon Tutorial' });
     expect(header).toBeInTheDocument();
+  });
+});
+
+describe('React Step 2 Tests', () => {
+  it('renders without crashing', () => {
+    render(<RepoPage />);
+    render(<LandingPage />);
+  });
+
+  it('RepoPage contains a RepoTable', () => {
+    render(<RepoPage />);
+    expect(
+      screen.getByRole('heading', { name: 'Carbon Repositories' })
+    ).toBeInTheDocument();
+    expect(screen.getByRole('table')).toBeInTheDocument();
+    expect(screen.getByRole('cell', { name: 'Repo 1' })).toBeInTheDocument();
   });
 });
